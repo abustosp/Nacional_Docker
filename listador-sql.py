2955ccf455275e6660811f9cc1088ac92ff34dab
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2024-05-31 15:16:25 -0300

    chore: Generate SQL import and creation scripts

--- listador-sql.py
+++ listador-sql.py
@@ -6,7 +6,12 @@ import re
 directorio = "master"
 archivos = os.listdir(directorio)
 archivos = [archivo for archivo in archivos if archivo.endswith('.sql')]
+# Eliminar el archivo creardatabases.sql si existe
+if 'creardatabases.sql' in archivos:
+    archivos.remove('creardatabases.sql')
 archivos = [os.path.join(directorio, archivo) for archivo in archivos]
+crear = []
+importar = []
 for i in archivos:
     # imprimir la database (es lo que precede a "Database: ") (está dentro de las primeras 5 línes)
     i = i.replace('\\', '/')
@@ -16,5 +21,16 @@ for i in archivos:
             if 'Database: ' in linea:
                 database = re.search(r'Database: (.+)', linea).group(1)
                 break
-    # print(f"{i.split('/')[-1]}:{database}")
-    print(f'docker exec -ti mysql bash -c "mysql -uroot -proot {database} < master/{i.split("/")[-1]}"')
+
+    # agregar a importar la línea f'docker exec -ti mysql bash -c "mysql -uroot -proot {database.replace(" ", "_")} < master/{i.split("/")[-1]}"'
+    importar.append(f'docker exec -ti mysql bash -c "mysql -uroot -proot {database.replace(" ", "_")} < master/{i.split("/")[-1]}"')
+    # agregar a crear la line f'CREATE DATABASE IF NOT EXISTS {database.replace(" ", "_")}'
+    crear.append(f'CREATE DATABASE IF NOT EXISTS {database.replace(" ", "_")};')
+
+# Exportar crear a sql
+with open('master/creardatabases.sql', 'w') as f:
+    f.write("\n".join(crear))
+    
+# Exportar importar a sh
+with open('importar.sh', 'w') as f:
+    f.write("\n".join(importar))

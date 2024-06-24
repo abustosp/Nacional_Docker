50cacaba3181dc5972a48f35232983330b5d6524
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2024-06-23 22:29:32 -0300

    chore: Update installation script to fix python-dotenv installation

--- installation.sh
+++ installation.sh
@@ -9,7 +9,7 @@ sudo apt update
 
 # Instalar python-dotenv
 echo "Instalando python-dotenv en un entorno virtual..."
-sudo apt install python3.12-venv -y
+sudo apt install python3-venv -y
 python3 -m venv .venv
 source ./.venv/bin/activate
 sudo apt install python3-pip -y
